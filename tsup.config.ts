e4b00bfffed2ed8498d6ab6caeac06d154b040cb
Robin Sabhaya <robin47ppsv@gmail.com>
2025-08-04 19:17:37 +0530

    - changes in tsup.config for file path

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/server.ts'],      
+  entry: ['./src/server.ts'],      
   format: ['esm', 'cjs'],       
-  dts: true,                     
+  dts: false,                     
   splitting: false,              
-  sourcemap: true,               
+  sourcemap: false,               
   clean: true,                   
   minify: true,                 
   outDir: 'dist',                
