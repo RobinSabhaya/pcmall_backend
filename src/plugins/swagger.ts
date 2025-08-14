f855f8f7310721012b0966332aab056990215f2c
Robin Sabhaya <robin47ppsv@gmail.com>
2025-08-14 20:20:19 +0530

    - set example in API docs

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -3,6 +3,31 @@ import { FastifyInstance } from 'fastify';
 import swagger from '@fastify/swagger';
 import swaggerUI from '@fastify/swagger-ui';
 import { config } from '../config/config';
+import { z } from 'zod'
+import { JSONSchema } from 'zod/v4/core';
+
+function getExampleFromSchema(schema: JSONSchema.BaseSchema): Record<string,string> {
+  const example: Record<string,string> = {};
+  for (const [key, value] of Object.entries(schema.properties as Record<string, JSONSchema.BaseSchema>)) {
+    example[key] = value.type || 'unknown';
+    }
+    return example;
+}
+
+function getExampleValue(schema: any): any {
+  switch (schema._def?.typeName) {
+    case 'ZodString':
+      return 'string';
+    case 'ZodNumber':
+      return 123;
+    case 'ZodBoolean':
+      return true;
+    case 'ZodArray':
+      return [getExampleValue(schema._def.type)];
+    default:
+      return 'example';
+  }
+}
 
 export default fp(async (fastify: FastifyInstance) => {
   await fastify.register(swagger, {
@@ -27,6 +52,21 @@ export default fp(async (fastify: FastifyInstance) => {
           },
         },
       },
+    },
+    transform: ({ schema, url }) => {
+      if (schema?.body && typeof schema?.body === 'object') {
+        (schema.body as Record<string, object>).example = getExampleFromSchema(z.toJSONSchema(schema?.body as z.ZodSchema));        
+      }
+
+      if (schema?.params && typeof schema?.params === 'object') {
+        (schema.params as Record<string, object>).example = getExampleFromSchema(z.toJSONSchema(schema?.params as z.ZodSchema));        
+      }
+
+      if (schema?.querystring && typeof schema?.querystring === 'object') {
+        (schema.querystring as Record<string, object>).example = getExampleFromSchema(z.toJSONSchema(schema?.querystring as z.ZodSchema));        
+      }
+      
+      return { schema, url };
     },
   });
 
