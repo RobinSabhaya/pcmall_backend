7d333af5b7e716d8adb074eece2b4109796ff8b6
Robin Sabhaya <robin47ppsv@gmail.com>
2025-10-27 11:01:32 +0530

    revert(category): revert category test file

--- test/integration/category/category.route.test.ts
+++ test/integration/category/category.route.test.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from 'fastify';
-import { afterAll, beforeAll, describe, test } from 'vitest';
+import { status as httpStatus } from 'http-status';
+import { afterAll, beforeAll, describe, expect, test } from 'vitest';
 
 import buildApp from '@/app';
 
+import { withAuth } from '../../helpers/function.helper';
+import { makeRequest } from '../../helpers/request.helper';
+import { expectSuccessResponse } from '../../helpers/response';
 import { disconnectDatabase, setupDatabase } from '../../helpers/setupDatabase';
+
+import { IGetAllCategoriesResponse } from './category.type';
 
 describe('Category route Integration Tests', () => {
   let app: FastifyInstance;
@@ -24,18 +30,18 @@ describe('Category route Integration Tests', () => {
   describe('GET /all', () => {
     test('should return 200 for valid GET request', async () => {
       // make request
-      // const response = await makeRequest<IGetAllCategoriesResponse>(
-      //   app,
-      //   'GET',
-      //   `/v1/category/all`,
-      //   {
-      //     headers: withAuth(),
-      //   }
-      // );
+      const response = await makeRequest<IGetAllCategoriesResponse>(
+        app,
+        'GET',
+        `/v1/category/all`,
+        {
+          headers: withAuth(),
+        }
+      );
       // test cases
-      // expectSuccessResponse(response, httpStatus.OK);
+      expectSuccessResponse(response, httpStatus.OK);
       // check at least one item exists
-      // expect(response.body.data).toBeDefined();
+      expect(response.body.data).toBeDefined();
     });
   });
 });
